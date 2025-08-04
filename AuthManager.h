bca78197206291585e9a57d90c11b2bad6d69fdb
nffdev <nightfrost2discord@outlook.fr>
2025-08-04 14:55:51 +0200

    feat(app): added helloworld function

---
+++ AuthManager.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const char* fnHelloWorld();
+
+void fnCPPLibrary();
+
+#ifdef __cplusplus
+}
+#endif
--- CPP-Library.cpp
+++ CPP-Library.cpp
@@ -3,8 +3,16 @@
 
 #include "pch.h"
 #include "framework.h"
+#include "AuthManager.h"
 
 // TODO: This is an example of a library function
 void fnCPPLibrary()
 {
+    // Original function implementation
+}
+
+// Function that returns Hello World for testing library integration
+const char* fnHelloWorld()
+{
+    return "Hello World";
 }
